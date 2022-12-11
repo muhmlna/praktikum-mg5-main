e455716ac07ab78c719329b2d32af54cd72454ce
muhmlna <92241534+muhmlna@users.noreply.github.com>
2022-12-11 23:14:48 +0700

    Add files via upload

---
+++ praktikum-mg5-main/Assets/Scripts/data.js
@@ -0,0 +1,93 @@
+const Mhs = [
+    {
+        nama: 'Hendri Aldi Zulfan',
+        nim: 120140186,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Fer D. Sam',
+        nim: 123111177,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Moron Arloji',
+        nim: 121155001,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Hendra Belmawan',
+        nim: 120140180,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Fera Riantasa',
+        nim: 123111179,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Upin Bersaudara',
+        nim: 121155045,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Seno Melati',
+        nim: 120140102,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Andanan Andi',
+        nim: 123111077,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Pekor Way Halim',
+        nim: 121155122,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Muhammad Muslim Nur Wahyudi',
+        nim: 120140008,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Jend. Ferdi Sambo',
+        nim: 123111054,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Jordi Asia',
+        nim: 121155100,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Jennifer',
+        nim: 120140005,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Mertani Kaya',
+        nim: 123111099,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Perkara Alba',
+        nim: 121155190,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Ridho Restu',
+        nim: 120140044,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Rizky Rizal',
+        nim: 123111023,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Mariposa Talaban',
+        nim: 121155125,
+        prodi: 'SI'
+    }
+];
+export {Mhs};
